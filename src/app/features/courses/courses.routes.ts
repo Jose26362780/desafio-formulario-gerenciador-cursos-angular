3c565114cea57a22bafdf0ac2fb9e14bd702d4fa
Jose Martinez <juniorjose1925@gmail.com>
2026-05-23 09:08:27 -0300

    refactor: Ajustar configuração de rotas

--- src/app/features/courses/courses.routes.ts
+++ src/app/features/courses/courses.routes.ts
@@ -5,25 +5,31 @@ export const COURSE_ROUTES: Routes = [
     path: '',
     pathMatch: 'full',
     loadComponent: () =>
-      import('./page/features/courses/pages/course-list/course-list').then((m) => m.CourseList),
+      import('./page/features/courses/pages/course-list/course-list.component').then(
+        (m) => m.CourseListComponent,
+      ),
   },
   {
     path: 'new',
     loadComponent: () =>
-      import('./page/features/courses/pages/course-form/course-form').then((m) => m.CourseForm),
+      import('./page/features/courses/pages/course-form/course-form.component').then(
+        (m) => m.CourseFormComponent,
+      ),
     data: { mode: 'create' },
   },
   {
     path: ':id',
     loadComponent: () =>
-      import('./page/features/courses/pages/course-detail/course-detail').then(
-        (m) => m.CourseDetail,
+      import('./page/features/courses/pages/course-details/course-details.component').then(
+        (m) => m.CourseDetailsComponent,
       ),
   },
   {
     path: ':id/edit',
     loadComponent: () =>
-      import('./page/features/courses/pages/course-form/course-form').then((m) => m.CourseForm),
+      import('./page/features/courses/pages/course-form/course-form.component').then(
+        (m) => m.CourseFormComponent,
+      ),
     data: { mode: 'edit' },
   },
 ];
