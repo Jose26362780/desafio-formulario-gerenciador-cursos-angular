48aec7e6c0ae4212b22ef5d91ef01e11d0a2f028
Jose Martinez <juniorjose1925@gmail.com>
2026-05-24 00:24:42 -0300

    Feat: Iniciando criar paginado course-list

--- src/app/features/courses/page/features/courses/pages/course-list/course-list.component.ts
+++ src/app/features/courses/page/features/courses/pages/course-list/course-list.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CourseService } from '../../../../../../../core/services/course.service';
 
 @Component({
   selector: 'app-course-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './course-list.component.html',
-  styleUrl: './course-list.component.css',
 })
 export class CourseListComponent {
-
+  private readonly courseService = inject(CourseService);
 }
