35337e8e9e0cf0399ab87834ea81776b8e040a15
Jose Martinez <juniorjose1925@gmail.com>
2026-05-27 09:49:24 -0300

    Feat: Inicializar componente card

---
+++ src/app/features/courses/components/course-card/course-card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-card',
+  imports: [],
+  templateUrl: './course-card.component.html',
+})
+export class CourseCardComponent {}
