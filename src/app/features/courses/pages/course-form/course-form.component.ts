d8e0f5872cba41d18ae7da78acdbe64200b633ae
Jose Martinez <juniorjose1925@gmail.com>
2026-06-15 00:07:27 -0300

    fix: Finalizado  component form-modules

--- src/app/features/courses/pages/course-form/course-form.component.ts
+++ src/app/features/courses/pages/course-form/course-form.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CourseFormSectionOneInfoComponent } from '../../components/course-form-section-one-info/course-form-section-one-info.component';
+import { CourseFormSectionTwoModulesComponent } from '../../components/course-form-section-two-modules/course-form-section-two-modules.component';
 
 @Component({
   selector: 'app-course-form',
   standalone: true,
-  imports: [CourseFormSectionOneInfoComponent],
+  imports: [CourseFormSectionOneInfoComponent, CourseFormSectionTwoModulesComponent],
   templateUrl: './course-form.component.html',
 })
 export class CourseFormComponent {}
