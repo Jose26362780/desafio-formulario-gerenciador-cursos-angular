b12146390f33abc52cb60ac1539f202c802dd3dd
Jose Martinez <juniorjose1925@gmail.com>
2026-06-19 11:29:55 -0300

    fix: Ajuste nos card de editar curso e alinhamento e criação de botoes

--- src/app/features/courses/pages/course-form-edit/course-form-edit.component.ts
+++ src/app/features/courses/pages/course-form-edit/course-form-edit.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CourseFormSectionOneInfoComponent } from '../../components/course-form-section-one-info/course-form-section-one-info.component';
+import { CourseFormSectionTwoModulesComponent } from '../../components/course-form-section-two-modules/course-form-section-two-modules.component';
 
 @Component({
   selector: 'app-course-form-edit',
   standalone: true,
-  imports: [],
+  imports: [CourseFormSectionOneInfoComponent, CourseFormSectionTwoModulesComponent],
   templateUrl: './course-form-edit.component.html',
 })
 export class CourseFormEditComponent {}
